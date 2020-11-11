6ea0301dce8f8514d15a7de6d70af354f6b53a2a
shlomicoh <shlomi.cohen@campus.technion.ac.il>
2020-11-11 19:42:31 +0200

    contenier before test

--- src/homework0/BallContainer.java
+++ src/homework0/BallContainer.java
@@ -1,20 +1,18 @@
 package homework0;
 import java.util.ArrayList;
-
-
 /**
- * A container that can be used to contain Balls. A given Ball may only
- * appear in a BallContainer once. Each container has a size, and can only contain balls up to the size of the container.
+ * A container that can be used to contain Balls. A given homework0.Ball may only
+ * appear in a homework0.BallContainer once. Each container has a size, and can only contain balls up to the size of the container.
  */
 public class BallContainer {
-    private static final int EMPTY = 0;
     private double containerSize;
     private double  containerCurrentVolume;
     private ArrayList<Ball> myBallsList;
-            /**
-             * @requires   res containerSize > 0
-             * @effects Creates a new BallContainer with the size of containerSize.
-             */
+    private final int EMPTY = 0;
+    /**
+     * @requires containerSize > 0
+     * @effects Creates a new homework0.BallContainer with the size of containerSize.
+     */
     public BallContainer(double containerSize) {
         if(containerSize>0) {
             this.containerSize = containerSize;
@@ -22,23 +20,37 @@ public class BallContainer {
             this.myBallsList = new ArrayList<Ball>();
         }
     }
-// first commit hello world
+
 
     /**
      * @modifies this
      * @effects Adds ball to the container.
      * @return true if ball was successfully added to the container,
-     * 		   i.e. ball is not already in the container and if adding ball does not cause 
-	 *		   the total volume of the balls in the container to exceed the size of	the container;
-	 * 		   false otherwise.
+     * 		   i.e. ball is not already in the container and if adding ball does not cause
+     *		   the total volume of the balls in the container to exceed the size of	the container;
+     * 		   false otherwise.
      */
-    public boolean add(Ball ball) { //todo: check size overflow
-		if(!contains(ball)) {
-	        containerCurrentVolume += ball.getVolume(); //todo check if ball is null
-	        myBallsList.add(ball); //todo: check if didnt succeed
-	        return true;
+    public boolean add(Ball ball) {
+        if((!contains(ball))&&(this.size()+1<=this.getContainerSize())) {
+            try {
+                myBallsList.add(ball);
+            }
+            catch(ClassCastException  e){
+                System.out.println("The class of the specified element prevents it from being added to this list");
+                return false;
+            }
+            catch (NullPointerException e){
+                System.out.println("Can not add NULL");
+                return false;
+            }
+            catch (IllegalArgumentException  e){
+                System.out.println("You try to add illegal argument");
+                return false;
+            }
+            containerCurrentVolume += ball.getVolume();
+            return true;
         }
-		else return false;
+        else return false;
     }
 
 
@@ -51,8 +63,14 @@ public class BallContainer {
      */
     public boolean remove(Ball ball) {
         if(contains(ball)) {
+            try{
+                myBallsList.remove(ball);
+            }
+            catch (NullPointerException e){
+                System.out.println("Can not remove NULL element");
+                return false;
+            }
             containerCurrentVolume -= ball.getVolume();
-            myBallsList.remove(ball); //todo: check if didnt succeed
             return true;
         }
         else return false;
@@ -66,8 +84,8 @@ public class BallContainer {
     public double getVolume() {
         return containerCurrentVolume;
     }
-	
-	/**
+
+    /**
      * @return the size of the container.
      */
     public double getContainerSize() {
@@ -78,7 +96,7 @@ public class BallContainer {
      * @return the number of Balls in the container.
      */
     public int size() {
-		return myBallsList.size();
+        return myBallsList.size();
     }
 
     /**
@@ -86,14 +104,14 @@ public class BallContainer {
      * @effects Empties the container, i.e., removes all its contents.
      */
     public void clear() {
-		myBallsList.clear();
-		containerCurrentVolume=EMPTY;
+        myBallsList.clear();
+        containerCurrentVolume=EMPTY;
     }
 
     /**
      * @return true if this container contains ball; false, otherwise.
      */
     public boolean contains(Ball ball) {
-		return myBallsList.contains(ball);
+        return myBallsList.contains(ball);
     }
 }
